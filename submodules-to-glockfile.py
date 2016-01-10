3a09a2ed29c05eaa66a010405f9e609951bf5238
Gregory Eremin <g@erem.in>
2016-01-10 20:30:02 +0300

    Add a script that prints submodules info in Glockfile compatible format

---
+++ submodules-to-glockfile.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import re
+import subprocess
+
+def main():
+    source = open(".gitmodules").read()
+    paths = re.findall(r"path = (.*)", source)
+
+    for path in paths:
+        print "{repo} {sha}".format(
+            repo = path[7:],
+            sha = path_sha1(path)
+        )
+
+def path_sha1(path):
+    cmd = "cd {} && git rev-parse HEAD".format(path)
+    sp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    sha1 = sp.stdout.read()[:-1]
+
+    return sha1
+
+if __name__ == "__main__": main()
